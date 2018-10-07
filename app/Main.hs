c2eea2730023792c57dd6a2272125d5b226255f4
Tom Busby <tom@busby.ninja>
2018-10-07 16:13:53 +0200

    Match on URL dirnames

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import Happstack.Server (Conf(port), nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad
+import Happstack.Server (Conf(port), nullConf, simpleHTTP, ok, dir)
 
 main :: IO ()
-main = simpleHTTP nullConf{port=9000} $ ok (toResponse "Hello, World!")
+main = simpleHTTP nullConf{port=9000} $ msum [
+        dir "hello"    $ ok "Hello, World!",
+        dir "hello" $ dir "moon" $ ok "Hello, Moon!", -- Will never be called since above succeeds
+        dir "goodbye" $ dir "moon" $ ok "Goodbye, Moon!",
+        dir "goodbye"  $ ok "Goodbye, World!"
+    ]
