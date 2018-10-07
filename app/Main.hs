f431f5c93ff8385ce740b0c6a152f82217733806
Tom Busby <tom@busby.ninja>
2018-10-07 16:17:12 +0200

    Use dirs function to match longer path

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import Control.Monad
-import Happstack.Server (Conf(port), nullConf, simpleHTTP, ok, dir)
+import Happstack.Server (Conf(port), nullConf, simpleHTTP, ok, dir, dirs)
 
 main :: IO ()
-main = simpleHTTP nullConf{port=9000} $ msum [
-        dir "hello"    $ ok "Hello, World!",
-        dir "hello" $ dir "moon" $ ok "Hello, Moon!", -- Will never be called since above succeeds
-        dir "goodbye" $ dir "moon" $ ok "Goodbye, Moon!",
-        dir "goodbye"  $ ok "Goodbye, World!"
-    ]
+main = do
+    putStrLn "Starting server..."
+    simpleHTTP nullConf{port=9000} $ msum [
+            dir "hello"    $ ok "Hello, World!",
+            dir "hello" $ dir "moon" $ ok "Hello, Moon!", -- Will never be called since above succeeds
+            dir "goodbye" $ dir "moon" $ ok "Goodbye, Moon!",
+            dir "goodbye"  $ ok "Goodbye, World!",
+            dirs "something/else" $ ok "Uses dirs function"
+        ]
