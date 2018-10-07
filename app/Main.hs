9274210514701ce4519afa89214a22c7c19fa96e
Tom Busby <tom@busby.ninja>
2018-10-07 16:40:24 +0200

    Set allowable path matches with FromReqURI instance

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,21 @@
 module Main where
 
 import Control.Monad
-import Happstack.Server (Conf(port), nullConf, simpleHTTP, ok, dir, dirs, path)
+import Data.Char (toLower)
+import Happstack.Server (Conf(port), FromReqURI(..), nullConf, simpleHTTP, ok, dir, dirs, path)
+
+data Subject = World | Haskell
+
+sayHello :: Subject -> String
+sayHello World   = "Hello, World!"
+sayHello Haskell = "Greetings, Haskell!"
+
+instance FromReqURI Subject where
+    fromReqURI sub =
+        case map toLower sub of
+            "haskell" -> Just Haskell
+            "world"   -> Just World
+            _         -> Nothing
 
 main :: IO ()
 main = do
@@ -12,5 +26,6 @@ main = do
             dir "goodbye" $ dir "moon" $ ok "Goodbye, Moon!",
             dir "goodbye"  $ ok "Goodbye, World!",
             dirs "something/else" $ ok "Uses dirs function",
-            dir "matchrest" $ path $ \s -> ok $ "Hello, " ++ s -- path matches up to next /
+            dir "matchrest" $ path $ \s -> ok $ "Hello, " ++ s, -- path matches up to next /
+            dir "fromrequri_instance" $ path $ \subject -> ok $ (sayHello subject)
         ]
